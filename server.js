aa3d1030f27b59d3ba031fc647bf625e7379f210
MaksTue <pro100max48@gmail.com>
2024-03-13 23:09:42 +0300

    fix

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
 import http from "http";
-import fs from "fs/promises";
+import fs from "fs";
 
-const writeLog = async (req, res) => {
-  try {
-    const log = `${new Date().toISOString()} - ${req.socket.remoteAddress} - ${
-      req.url
-    } - ${res.statusCode}\n`;
+const writeLog = (req, res) => {
+  const log = `${new Date().toISOString()} - ${req.socket.remoteAddress} - ${
+    req.url
+  } - ${res.statusCode}\n`;
 
-    await fs.appendFile("server.log", log);
-  } catch (error) {
-    console.error("Failed to log request:", error);
-  }
+  fs.appendFile("server.log", log, (err) => {
+    if (err) {
+      console.error("Failed to log request:", err);
+    }
+  });
 };
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer((req, res) => {
   let filePath = "." + req.url;
-
   if (filePath === "./") {
     filePath = "./index.html";
   }
 
-  try {
-    await fs.access(filePath, fs.constants.F_OK);
-  } catch (error) {
-    res.writeHead(404);
-    res.end("404 Not Found");
-    await writeLog(req, res);
-    return;
-  }
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+      writeLog(req, res);
+      return;
+    }
 
-  try {
-    const data = await fs.readFile(filePath);
-    res.writeHead(200);
-    res.end(data);
-  } catch (error) {
-    res.writeHead(500);
-    res.end("Internal Server Error");
-  } finally {
-    await writeLog(req, res);
-  }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Internal Server Error");
+        writeLog(req, res);
+        return;
+      }
+
+      res.writeHead(200);
+      res.end(data);
+
+      writeLog(req, res);
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
